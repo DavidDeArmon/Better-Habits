bafdd561e8e2e096e734699ebfd77f8da438404e
David DeArmon <david.dearmon@gmail.com>
2018-10-26 14:42:11 -0500

    changing dist path

--- server/index.js
+++ server/index.js
@@ -28,7 +28,7 @@ massive(process.env.CONNECTION_STRING).then(dbInstance => {
   app.set("db", dbInstance);
 });
 
-app.use(express.static(__dirname+'/../dist'))
+app.use(express.static(path.join(__dirname,'../dist/index.html')))
 //endpoints
 app.post("/api/moods", insertMood);
 app.delete("/api/moods/:id", deleteTodaysMood);
