eef6a71bddb0df4715d71ae7b62fa092d492a61c
David DeArmon <david.dearmon@gmail.com>
2018-12-04 22:04:35 -0600

    update image

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "../CSS/Dashboard.scss";
+import DavidsIconWhite from '../../../public/assets/DavidsIconWhite.svg'
 
 class Header extends React.Component {  
   render() {
     return (
         <div className="dashboardHeader">
-          <img id='icon' src='DavidsIconWhite.svg'/>
+          <img id='icon' src={DavidsIconWhite}/>
           <Link className="Link" to="/dashboard">
             Dashboard
           </Link>
